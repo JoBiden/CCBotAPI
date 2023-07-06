c45e6d518add3e84057e8e6f814318ce29e3ad44
Gryphon Blazier <blazierg@gmail.com>
2023-07-05 17:54:39 -0700

    Update index.js

--- CCBotAPI/index.js
+++ CCBotAPI/index.js
@@ -1,7 +1,7 @@
 var FChatLib = require('./libfchat.js');
 
 var options = {
-	username: "JoBiden", password: "Game5Potato",
+	username: "JoBiden", password: "",
 	character: "Chateau Contract", master: "Queen Contract",
 	room: "adh-fd25961c49e29c5de795", cname: "CCBot", cversion: "0.5"
 };
